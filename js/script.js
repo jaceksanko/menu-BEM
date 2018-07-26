60f8fd05df7a6acc892f2948e9ade74339bc5f4b
Jacek Sańko <jacek.sanko@gmail.com>
2018-07-26 15:14:53 +0200

    Change elements to BEM methodology

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-const iconOpenClose = document.getElementById('js-menu-icon-open-close');
+const iconOpenClose = document.getElementById('js-menu-icon__open-close');
 const menuList = document.querySelectorAll('nav ul');
 const menuOpenClose = document.querySelector('nav');
-const body = document.querySelector('header');
-const subMenuNav = document.querySelector('.sub-menu-nav ');
+//const body = document.querySelector('.header');
+const subMenuNav = document.querySelector('.menu__nav__sub-menu ');
 
 let hidenMenu = () => {
   menuList[0].classList.remove('fadeInDown');
@@ -10,7 +10,7 @@ let hidenMenu = () => {
   menuList[0].classList.toggle('fadeOutUp');
   menuList[1].classList.toggle('fadeOutUp');
   setTimeout(() => {
-    menuOpenClose.classList.add('menu-open-close');
+    menuOpenClose.classList.add('js-menu');
     menuList[0].classList.remove('fadeOutUp');
     menuList[1].classList.remove('fadeOutUp');
   }, 500) 
@@ -19,8 +19,8 @@ let hidenMenu = () => {
 let clickMenuOut = function(e) {
   // if one of these conditions take false function does not running
   if ( e.target.parentNode.parentNode.parentNode != menuOpenClose && e.target.parentNode.parentNode != menuOpenClose && e.target.parentNode.parentNode != subMenuNav ) {
-    if (iconOpenClose.classList.contains('line-menu-icon-move') == true) {
-      iconOpenClose.classList.remove('line-menu-icon-move');
+    if (iconOpenClose.classList.contains('menu-icon__line--move') == true) {
+      iconOpenClose.classList.remove('menu-icon__line--move');
       hidenMenu();
     }
   }
@@ -30,15 +30,15 @@ window.addEventListener('click', clickMenuOut);
 
 iconOpenClose.addEventListener('click', function(e) {
   e.stopPropagation();
-  if (this.classList.contains('line-menu-icon-move') == false) {
+  if (this.classList.contains('menu-icon__line--move') == false) {
     menuList[0].classList.remove('fadeOutUp');
     menuList[1].classList.remove('fadeOutUp');
-    this.classList.toggle('line-menu-icon-move');
+    this.classList.toggle('menu-icon__line--move');
     menuList[0].classList.toggle('fadeInDown');
     menuList[1].classList.toggle('fadeInDown');
-    menuOpenClose.classList.toggle('menu-open-close');
+    menuOpenClose.classList.toggle('js-menu');
   } else {
-    this.classList.remove('line-menu-icon-move');
+    this.classList.remove('menu-icon__line--move');
     hidenMenu();
     
   }
